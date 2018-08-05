9b83d864be902b3e75b3fba4073b3b26b19c7302
Alex Sazonov <karl93rus@gmail.com>
2018-08-06 00:40:19 +0300

    Now the app can add new tasks to the list. But it still can not show the task content. Back to the drawing board!

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
+// Variables section
+
 const modalWindow = document.getElementsByClassName("modal")[0];
 const modalCloseBtn = document.getElementById("modalClose");
 const modalClearBtn = document.getElementById("modalClear");
 const newHeaderInput = document.getElementById("newHeaderInput");
 const newContentInput = document.getElementById("newContentInput");
+const newSaveBtn = document.getElementById("newSave");
 const createBtn = document.getElementById("createBtn");
+const taskList = document.getElementById("tasks");
+const itemContent = document.getElementById("item-content");
+
+// Events section
 
 createBtn.addEventListener("click", () => {
 // Reveal moadal window by CREATE btn click
@@ -21,4 +28,14 @@ modalClearBtn.addEventListener("click", () => {
 // Clears all input fields
     newHeaderInput.value = "";
     newContentInput.value = "";
+});
+
+newSaveBtn.addEventListener("click", () => {
+//Saves and adds new task to the list
+    let newTaskHeader = newHeaderInput.value;
+    let newTaskItem = document.createElement("li");
+    newTaskItem.textContent = newTaskHeader;
+    newTaskItem.className = "task-item";
+    taskList.appendChild(newTaskItem);
+    modalWindow.className = "modal";
 });
