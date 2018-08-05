bffd8c7178e7d20fbb5f7ab196e0755f116fa8df
Alex Sazonov <karl93rus@gmail.com>
2018-08-05 23:41:40 +0300

    Comment now are inside funtions` bodies

--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@ const modalWindow = document.getElementsByClassName("modal")[0];
 const modalCloseBtn = document.getElementById("modalClose");
 const createBtn = document.getElementById("createBtn");
 
-// Reveal moadal window by CREATE btn click
 createBtn.addEventListener("click", () => {
+// Reveal moadal window by CREATE btn click
     modalWindow.className = "modalRev";
 });
 
-// Close modal window and save data
 modalCloseBtn.addEventListener("click", () => {
+// Close modal window and save data
     modalWindow.className = "modal";
 })
