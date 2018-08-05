3aacc2e29dee799e18292d40f6945beb20501408
Alex Sazonov <karl93rus@gmail.com>
2018-08-05 23:59:16 +0300

    Made clear btn do its work :). And fixed a bug with a textarea component.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
 const modalWindow = document.getElementsByClassName("modal")[0];
 const modalCloseBtn = document.getElementById("modalClose");
+const modalClearBtn = document.getElementById("modalClear");
+const newHeaderInput = document.getElementById("newHeaderInput");
+const newContentInput = document.getElementById("newContentInput");
 const createBtn = document.getElementById("createBtn");
 
 createBtn.addEventListener("click", () => {
 // Reveal moadal window by CREATE btn click
     modalWindow.className = "modalRev";
+    newHeaderInput.value = "";
+    newContentInput.value = "";
 });
 
 modalCloseBtn.addEventListener("click", () => {
 // Close modal window and save data
     modalWindow.className = "modal";
-})
+});
+
+modalClearBtn.addEventListener("click", () => {
+// Clears all input fields
+    newHeaderInput.value = "";
+    newContentInput.value = "";
+});
