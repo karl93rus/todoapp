1503c8a41fb26a105d245dc7b52509045ea2e047
Alex Sazonov <karl93rus@gmail.com>
2018-08-07 21:41:12 +0300

    Added EDIT functionality. Although it has some issues, it works in general. Will fix it soon.

--- js/main.js
+++ js/main.js
@@ -1,18 +1,33 @@
 // ------------------------------------------- Variables section
 
+// WINDOWS
 const modalWindow = document.getElementsByClassName("modal")[0];
-const modalCloseBtn = document.getElementById("modalClose");
-const modalClearBtn = document.getElementById("modalClear");
+const editWindow = document.getElementsByClassName("edit")[0];
+
+// INPUTS
 const newHeaderInput = document.getElementById("newHeaderInput");
 const newContentInput = document.getElementById("newContentInput");
+const editTextArea = document.getElementsByClassName("edit-task-text")[0];
+
+// BUTTONS
+const modalCloseBtn = document.getElementById("modalClose");
+const modalClearBtn = document.getElementById("modalClear");
 const newSaveBtn = document.getElementById("newSave");
 const createBtn = document.getElementById("createBtn");
 const deleteBtn = document.getElementById("delBtn");
-const taskList = document.getElementById("tasks");
-const itemContent = document.getElementById("itcont");
+const editBtn = document.getElementById("editBtn");
+const editCloseBtn = document.getElementById("editCancelBtn");
+const editSaveBtn = document.getElementById("editSaveBtn");
+
+// OTHER
+const taskList = document.getElementById("tasks"); // <ul> to store task items
+const itemContent = document.getElementById("itcont"); // area on the right to display task content
 
 // Array for storing all tasks
 let taskContentArray = [];
+
+// To store the item 
+let currentTaskObj;
 
 // ------------------------------------------- Functions section
 
@@ -23,6 +38,7 @@ function TaskObj(head, cont) {
 }
 
 function taskItemShow(event) {
+// Highlights selected item and shows the item content
     let item = event.target;
     let kids = taskList.children;
 
@@ -86,7 +102,7 @@ deleteBtn.addEventListener("click", () => {
         alert("Nothing to delete");
         return;
     }
-    
+
     for(let i = 0; i < taskContentArray.length; i++) {
         if(taskContentArray[i].header === taskList.getElementsByClassName("task-item-selected")[0].textContent) {
             taskContentArray.splice(i, 1);
@@ -99,3 +115,51 @@ deleteBtn.addEventListener("click", () => {
 
 // Task items event to hightlight and show content details
 taskList.addEventListener("click", taskItemShow);
+
+editBtn.addEventListener("click", () => {
+// Inits the EDIT window
+    if(taskList.children.length ==0 || itemContent.textContent === "") {
+        alert("Nothing to edit");
+        return;
+    } else {
+        editWindow.className = "editRev";
+        let selectedItem = document.getElementsByClassName("task-item-selected")[0];
+
+        for(let i = 0; i < taskContentArray.length; i++) {
+            if(taskContentArray[i].header === selectedItem.textContent) {
+                editTextArea.textContent = taskContentArray[i].content;
+                currentTaskObj = taskContentArray[i];
+                return;
+            }
+        }
+    }
+});
+
+editCloseBtn.addEventListener("click", () => {
+// Closes edit window without any savings
+    editWindow.className = "edit";
+});
+
+editTextArea.addEventListener("keypress", () => {
+// Tracks if the item content was changed and activates the SAVE button
+    editSaveBtn.className = "edit-btns";
+});
+
+editTextArea.addEventListener("change", () => {
+// Tracks the changes in the textarea and stores them into temp obj
+    currentTaskObj.content = editTextArea.value;
+});
+
+editSaveBtn.addEventListener("click", () => {
+// Saves the changes 
+    for(let i = 0; i < taskContentArray.length; i++) {
+        if(taskContentArray[i].header === currentTaskObj.header) {
+            taskContentArray[i] = currentTaskObj;
+            itemContent.textContent = taskContentArray[i].content;
+            break;
+        }
+    }
+
+    editSaveBtn.className = "edit-btns-inactive";
+    editWindow.className = "edit";
+});
